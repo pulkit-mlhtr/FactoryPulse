2cae717ce5166f94389cefbce0a9ed0946852848
pulkit-mlhtr <pulkit_mlhtr@yahoo.com>
2026-03-14 22:15:24 +0100

    Add FactoryPulse.Infrastructure project with EF Core setup
    This project will be responsible to connect with database and perform CRUD operations.

---
+++ FactoryPulse/FactoryPulse.Infrastructure/Context/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using FactoryPulse.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryPulse.Infrastructure.Context
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<Equipment> Equipments { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<EquipmentStateLog> EquipmentStateLogs { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+     : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}
---
+++ FactoryPulse/FactoryPulse.Infrastructure/Repository/Interface/IEquipmentRepository.cs
@@ -0,0 +1,16 @@
+﻿using FactoryPulse.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryPulse.Infrastructure.Repository.Interface
+{
+    public interface IEquipmentRepository
+    {
+        Task<List<Equipment>> GetAllAsync();
+
+        Task<Equipment?> GetByIdAsync(Guid id);
+
+        Task UpdateAsync(Equipment equipment);
+    }
+}
